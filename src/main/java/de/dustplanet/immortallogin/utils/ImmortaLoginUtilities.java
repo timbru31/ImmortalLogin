d275345de8281b80a942cb7b653f808f323886aa
Tim Brust <github@timbrust.de>
2020-02-25 23:25:06 +0100

    feat: updates bStats

--- src/main/java/de/dustplanet/immortallogin/utils/ImmortaLoginUtilities.java
+++ src/main/java/de/dustplanet/immortallogin/utils/ImmortaLoginUtilities.java
@@ -19,6 +19,7 @@ import de.dustplanet.immortallogin.utils.Updater.UpdateResult;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 public class ImmortaLoginUtilities {
+    private static final int BSTATS_PLUGIN_ID = 683;
     private ImmortalLogin plugin;
 
     public ImmortaLoginUtilities(ImmortalLogin instance) {
@@ -51,7 +52,7 @@ public class ImmortaLoginUtilities {
 
     @SuppressWarnings("unused")
     public void trackMetrics() {
-        new Metrics(plugin);
+        new Metrics(plugin, BSTATS_PLUGIN_ID);
     }
 
     public void loadConfig() {
